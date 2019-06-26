9b323cf4ae9c2a92a4ea3201589e6de592ab06a3
Michelle Saad <git@michsa.me>
2019-06-25 23:57:24 -0400

    Babel update (artisanal hand-coded)

--- src/string.js
+++ src/string.js
@@ -52,5 +52,5 @@ export let uniqueStringWith = _.curry((cachizer, initialKeys) => {
   return f
 })
 
-export let uniqueString = (arr = []) =>
+export let uniqueString = (arr = {}) =>
   uniqueStringWith(_.countBy(_.identity), arr)
